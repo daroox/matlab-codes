5ea1aef6e9b4aefd3e34aa380684bb0770b580fb
daroox <32034038+daroox@users.noreply.github.com>
2017-09-28 17:10:00 +0200

    Update least square nonlinear.m

--- snippets/least square nonlinear.m
+++ snippets/least square nonlinear.m
@@ -1,31 +1,22 @@
-d=linspace(0,3) %creates 
+
+
+clear all; close all; clc
+
+tic
+d = linspace(0,3); % creates hundred evenly spaced pts betw 0 and 3 
 y = exp(-1.3*d) + 0.05*randn(size(d));
-plot (y)
-plot (y,'.')
-grid on
-randn(size(d))
-size(d)
-y = exp(-1.3*d) + 0.05*randn(1);
-plot (y,'.')
-randn(1)
-randn(1)*0.05
-y = exp(-1.3*d) + 0.05*randn(1);
-plot (y,'.')
-y = exp(-1.3*d) + 0.05*randn(size(d));
-plot (y,'.')
-plot (d,y,'.')
-plot (y,'.')
-plot (d,y,'.')
+
+% now define your guess function, that could fit the data! 
 fun=@(r)exp(-d*r)-y;
-x0=4;
-x=lsqnonlin(fun,x0)
-plot(d,y)
-plot(d,y,'ko')
-plot(d,y,'ko',d,exp(-d*x)
-plot(d,y,'ko',d,exp(-d*x))
+x0=4; % Arbitrarily choose an initial guess x0 = 4.
+
+% start the optimization function, which returns a value to x
+x = lsqnonlin(fun,x0)
+
 plot(d,y,'ko',d,exp(-d*x),'b-')
 legend('data','fit')
 xlabel('t')
 ylabel('exp(-tx)')
 xlabel('time')
 grid on
+fprintf ('code executed in %0.2f seconds',toc)
