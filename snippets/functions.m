ef677db4860a602f379e29355be240d6354cf346
daroox <32034038+daroox@users.noreply.github.com>
2017-09-29 09:54:36 +0200

    Create functions.m

---
+++ snippets/functions.m
@@ -0,0 +1,39 @@
+
+% writing a function has the following conventions
+% one function per script; functions defined in a function file are just locally available
+% the following function for example should be saved as stat.m
+% it takes one argument and returns two values
+
+function [m,s] = stat(x)
+n = length(x);
+m = sum(x)/n;
+s = sqrt(sum((x-m).^2/n));
+end
+
+% -----------------------------------------------------
+%% the following funtion should be saved as average.m
+
+function y = average(x)
+if ~isvector(x)
+    error('Input must be a vector')
+end
+y = sum(x)/length(x); 
+end
+
+% ------------------------------------------------
+
+
+%% some advanced stuff
+% Compute the value of the integrand at 2*pi/3.
+x = 2*pi/3;
+y = myIntegrand(x)
+
+% Compute the area under the curve from 0 to pi.
+xmin = 0;
+xmax = pi;
+f = @myIntegrand;
+a = integral(f,xmin,xmax)
+
+function y = myIntegrand(x)
+y = sin(x).^3;
+end
