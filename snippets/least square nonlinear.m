20645828588e41996a4e6c62c9447774de1c4239
daroox <32034038+daroox@users.noreply.github.com>
2017-09-29 09:45:22 +0200

    Update least square nonlinear.m

--- snippets/least square nonlinear.m
+++ snippets/least square nonlinear.m
@@ -1,22 +1,22 @@
 
 
-clear all; close all; clc
+clear all; close all; clc   % clc clears the screen
 
-tic
-d = linspace(0,3); % creates hundred evenly spaced pts betw 0 and 3 
-y = exp(-1.3*d) + 0.05*randn(size(d));
+tic   % starts a stopwatch to be stopped by the function toc
+d = linspace(0,3); % creates hundred evenly spaced pts betw 0 and 3   | this line is the independent variable
+y = exp(-1.3*d) + 0.05*randn(size(d));              %                 | this is so to say the measured quantity
 
-% now define your guess function, that could fit the data! 
+% now define your guess function, that could fit the data tendence ! 
 fun = @(r)exp(-d*r) - y;  % notice that the exp function is substracted by y! we are minimizing
 x0=4; % Arbitrarily choose an initial guess x0 = 4.
 
 % start the optimization function, which returns a value to x variable
 x = lsqnonlin(fun,x0)
 
-plot(d,y,'ko',d,exp(-d*x),'b-')
+plot(d,y,'dc',d,exp(-d*x),'b-')   % 'dc' d:diamond  c:cyan
 legend('data','fit')
 xlabel('t')
 ylabel('exp(-tx)')
 xlabel('time')
 grid on
-fprintf ('code executed in %0.2f seconds',toc)
+fprintf ('code executed in %0.2f seconds',toc)    % stops a stopwatch started by the function tic
