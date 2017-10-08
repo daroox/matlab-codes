2542e39089307d6b8a94b3836b3e5ce742be1bb1
daroox <32034038+daroox@users.noreply.github.com>
2017-10-08 15:55:39 +0200

    Update drawGaussian.m

--- snippets/functions/drawGaussian.m
+++ snippets/functions/drawGaussian.m
@@ -1,6 +1,9 @@
+%   this function draws a gaussian time profile that is Power Vs time of a laser pulse
+%   the input function is the fwhm of the pulse
+
 function z = drawGaussian(t_fwhm)
 
-t = -(0.50*t_fwhm+t_fwhm):0.1:(0.50*t_fwhm+t_fwhm);
+t = -(0.50*t_fwhm+t_fwhm):0.1:(0.50*t_fwhm+t_fwhm);   %  the range is determined by the fwhm input| 50% of the fwhm is added to both sides
 c =num2str(t_fwhm);
 tau = t_fwhm;
 LG = -4*log(2);
@@ -14,7 +17,7 @@ for k = 1:size(t,2)
 end
 
 plot (t,z)
-title(['Gaussian pulse with ', c, ' as' ])
+title(['Gaussian pulse with ', c, ' as' ])      % placeholders inside title are peculiar! 
 xlabel('Time (as)')
 ylabel('Power (normalized)')
 grid on
