e9f7a00fdce9152e6b09b6ec209370da7b0f91ea
daroox <32034038+daroox@users.noreply.github.com>
2017-10-10 09:56:26 +0200

    Create strkng_ft.m

---
+++ analysis/Fitting/strkng_ft.m
@@ -0,0 +1,33 @@
+function [fitresult, gof] = strkng_ft(x, y)
+%CREATEFIT(X,Y)
+%  Create a fit.
+%
+%  Data for 'untitled fit 1' fit:
+%      X Input : x
+%      Y Output: y
+%  Output:
+%      fitresult : a fit object representing the fit.
+%      gof : structure with goodness-of fit info.
+%
+%% Fit: 'untitled fit 1'.
+[xData, yData] = prepareCurveData( x, y );
+
+% Set up fittype and options.
+ft = fittype( 'm*x+b', 'independent', 'x', 'dependent', 'y' );
+opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+opts.Display = 'Off';
+opts.StartPoint = [0.946589212165641 0.913375856139019];
+
+% Fit model to data.
+[fitresult, gof] = fit( xData, yData, ft, opts );
+
+% Plot fit with data.
+figure( 'Name', 'untitled fit 1' );
+h = plot( fitresult, xData, yData );
+legend( h, 'y vs. x', 'untitled fit 1', 'Location', 'NorthEast' );
+% Label axes
+xlabel x
+ylabel y
+grid on
+
+
