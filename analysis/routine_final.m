01f556cad52ea718abd5b6d640fec63d5e7d562f
daroox <32034038+daroox@users.noreply.github.com>
2017-10-20 09:18:34 +0200

    Update routine_final.m

--- analysis/routine_final.m
+++ analysis/routine_final.m
@@ -13,25 +13,45 @@ files =dir('*.dat');
 s = size(files,1);
 fac = 1;     %where to start plotting traces
 myformat = '%3.4f \t %3.4f\n';
-fileName = 'streaked-CH1.txt';
+fileName = 'un-streaked-CH2.txt';
 
 for k = 0:s-1
-    datFile = sprintf('C1TOF1_Xe_zero crossing_Lower harmonic xuv thz0000%d.dat',k);
+    datFile = sprintf('C2unstreaked0000%d.dat',k);
          D = load(datFile);
          t = D(fac:end,1)*1e9; 
          values = D(fac:end,2)*-1000;
 %call function with variables
 [delay,energie_interpol,Intensitaet,N_interpol]=Time2EnergyStreaking(values,t,1,50);
 % put the above func into peakfit         
-[Fit,GOF,baselin,coeff,res,xi,yi]=peakfit([energie_interpol',N_interpol'],19.3,10,2,1,0,10, [22.3 1 23.5 1], 2);
+[Fit,GOF,baselin,coeff,res,xi,yi]=peakfit([energie_interpol',N_interpol'],19,10,2,1,0,10, [22.3 1 23.5 1], 2);
 P =[P;Fit];
 end
 % calc average of fwhm of peaks
 P
+half = length(P(:,1))/2; % for average of peak fwhm
+ % sort peaks by their position and NOT by their nr. col2 is peakposition
 P = sortrows(P,2);
 fileID = fopen(fileName,'W');
 fprintf(fileID,'%6s %12s\r\n','% av1','av2');%header
-fprintf(fileID,myformat,mean(P(1:3,4)),mean(P(4:6,4)));
+fprintf(fileID,myformat,mean(P(1:half,4)),mean(P(half+1:end,4)));
 fclose(fileID);
 P
 copyfile *.txt ..\..
+
+%           -------------------------------------------
+%           plot tof and energy
+%           ------------------------------------------
+%   figure(4);  
+%   subplot(2,1,1)
+%   plot(energie_interpol,N_interpol)
+%   title(datFile)
+%   xlabel('energy (eV)')
+%   ylabel(' arb units')
+%   legend('interpolated')
+%   grid on
+%   
+%   subplot(2,1,2) 
+%   plot(t,values)
+%    xlabel('time (ns)')
+%   ylabel(' arb units')
+%   grid
