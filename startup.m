82089d7abd31c10b10dfac051127eb56929f01d7
daroox <32034038+daroox@users.noreply.github.com>
2017-10-21 00:30:02 +0200

    Create startup.m

---
+++ startup.m
@@ -0,0 +1,4 @@
+For those who are interested in using cbrewer.m, here's what I've got in my startup.m:
+set(0,'DefaultAxesColorOrder',cbrewer('qual','Set2',8))
+set(0,'DefaultLineLineWidth',1.2)
+set(0,'DefaultFigureColormap',cbrewer('seq','YlOrRd',64));
