b6e7d11f9d0a0a295e130cbb04907c068bb28114
daroox <32034038+daroox@users.noreply.github.com>
2017-11-04 11:33:50 +0100

    Create plot colors.m

---
+++ snippets/plot colors.m
@@ -0,0 +1,17 @@
+%{
+Plots the data set, y vs x using red plus-es.
+Color and point options include
+yellow 'y'      circles 'o'
+magenta 'm'     Plus signs '+'
+cyan 'c'        Asterisks '*'
+red 'r'         Points '.'
+green 'g'       Crosses 'x'
+blue 'b'        Squares 's'
+white 'w'       Diamonds 'd'
+black 'k'       Upward Pointing Triangles '^'
+                Downward Pointing Triangles 'v'
+                Right Pointing Triangles '>'
+                Left Pointing Triangles '<'
+                Pentragrams 'p'
+                Hexagrams 'h'
+%} 
